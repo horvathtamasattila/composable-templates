785ee093f9382ad55a49b7ec2acbbd65ed878412
Horvath Tamas <tamas.horvath@makery.co>
2020-10-23 15:51:15 +0200

    add: app core with optional state

---
+++ Composable Architecture Core/Composable App Base.xctemplate/Default/___VARIABLE_appName___App.swift
@@ -0,0 +1,37 @@
+import ComposableArchitecture
+
+struct AppState: Equatable {
+    //Shared values
+
+    //Local viewstates
+    var ___VARIABLE_prefixName___Local = ___VARIABLE_moduleName___State.Local()
+}
+
+enum AppAction: Equatable {
+    case ___VARIABLE_prefixName___(action: ___VARIABLE_moduleName___Action)
+}
+
+extension AppState {
+    var ___VARIABLE_prefixName___: ___VARIABLE_moduleName___State {
+        get {
+            return ___VARIABLE_moduleName___State(
+                local: ___VARIABLE_prefixName___Local
+            )
+        } set {
+            ___VARIABLE_prefixName___Local = newValue.local
+        }
+    }
+}
+
+let appReducer: Reducer<AppState, AppAction, AppEnvironment> = Reducer.combine(
+    ___VARIABLE_prefixName___Reducer.pullback(
+        state: \.___VARIABLE_prefixName___,
+        action: /AppAction.___VARIABLE_prefixName___,
+        environment: { $0 }
+    ),
+    Reducer { state, action, environment in
+        switch action {
+        default: break
+        }
+        return .none
+})
